c9853f71d25219e4400fdcc0bd151696043c86b8
your name <your email>
2022-07-05 22:46:15 +0800

    首页布局80%

---
+++ src/views/HomeView.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="home">
+    <div class="common-layout">
+      <el-container>
+        <el-header>
+          <div class="logo">
+            <img src="../assets/images/pp_logo.png" alt="" class="logoIMG" />
+          </div>
+
+          <h2>皮皮后台管理系统</h2>
+
+          <div class="signOut">
+            <img src="../assets/images/user.jpg" alt="" />
+            <el-button type="danger">退出登录</el-button>
+          </div>
+        </el-header>
+        <el-container>
+          <el-aside width="200px">
+            <el-menu
+              active-text-color="#ffd04b"
+              background-color="#96b1d9"
+              class="el-menu-vertical-demo"
+              default-active="2"
+              text-color="#fff"
+              @open="handleOpen"
+              @close="handleClose"
+            >
+              <el-menu-item index="2">
+                <el-icon><icon-menu /></el-icon>
+                <span>商品列表</span>
+              </el-menu-item>
+            </el-menu>
+          </el-aside>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {},
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  .common-layout {
+    width: 100%;
+    height: 100%;
+    .el-container {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+.el-header {
+  background-color: #3b6ec7;
+  height: 80px;
+  padding: 0;
+  display: flex;
+  .logo {
+    width: 200px;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .logoIMG {
+    width: 60px;
+    height: 60px;
+  }
+  h2 {
+    flex: 1;
+    text-align: center;
+    line-height: 80px;
+    color: #fff;
+  }
+  .signOut {
+    width: 300px;
+    height: 80px;
+    line-height: 80px;
+    font-size: 24px;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      overflow: hidden;
+      margin-right: 20px;
+    }
+  }
+}
+
+.el-aside {
+  // width: 100%;
+  height: 100%;
+  background-color: #96b1d9;
+}
+.el-main {
+  width: 100%;
+  height: 100%;
+  background: #d0dfef;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="login">
+    <el-form
+      ref="ruleFormRef"
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <h1>皮皮后台管理系统</h1>
+      <el-form-item label="账号" prop="username">
+        <el-input v-model="ruleForm.username" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="ruleForm.password" type="password" autocomplete="off" />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button class="loginBtn" type="primary" @click="submitForm(ruleFormRef)">登录</el-button>
+        <el-button class="loginBtn" @click="resetForm(ruleFormRef)">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs, ref } from 'vue'
+import { LoginData } from '../type/login'
+import type { FormInstance } from 'element-plus'
+import { login } from '../request/api'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  setup() {
+    const data = reactive(new LoginData())
+    const rules = {
+      username: [
+        { required: true, message: '请输入你的账号', trigger: 'blur' },
+        { min: 5, max: 10, message: '账号长度5-10', trigger: 'blur' },
+      ],
+      password: [
+        { required: true, message: '请输入你的密码', trigger: 'blur' },
+        { min: 6, max: 10, message: '账号长度6-10', trigger: 'blur' },
+      ],
+    }
+    // 登录
+    const ruleFormRef = ref<FormInstance>()
+    const router = useRouter() //相当于$router
+    const submitForm = async (formEl: FormInstance | undefined) => {
+      if (!formEl) return
+      // 对表单的内容进行验证
+      // valid布尔类型，为true表示验证成功,反之
+      await formEl.validate((valid, fields) => {
+        if (valid) {
+          // console.log('submit!')
+          login(data.ruleForm).then(res => {
+            console.log('结果', res)
+            localStorage.setItem('token', res.data.token)
+            router.push('/')
+          })
+        } else {
+          console.log('error submit!', fields)
+        }
+      })
+    }
+
+    // 重置
+    const resetForm = () => {
+      data.ruleForm.username = ''
+      data.ruleForm.password = ''
+    }
+    return { ...toRefs(data), rules, ruleFormRef, resetForm, submitForm }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background: url('../assets/images/bg.jpeg');
+  // background-size: contain;
+  position: relative;
+  .demo-ruleForm {
+    position: absolute;
+    right: 200px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 450px;
+    height: 500px;
+    background-color: #fff;
+    border-radius: 20px;
+    padding: 20px;
+    padding-top: 100px;
+    padding-right: 50px;
+    overflow: hidden;
+    // box-sizing: border-box;
+    h1 {
+      width: 100%;
+      height: 100px;
+      text-align: center;
+      color: #1970d3;
+    }
+    .el-button {
+      /* margin-left: 12px; */
+      margin: 0;
+    }
+    .loginBtn {
+      width: 100%;
+      height: 40px;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
